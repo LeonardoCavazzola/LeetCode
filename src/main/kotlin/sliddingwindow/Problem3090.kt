4f3e2ae0545e5a39df0c1872c5b8e8536ef61510
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2025-09-26 00:35:12 -0300

    add 3090

---
+++ src/main/kotlin/sliddingwindow/Problem3090.kt
@@ -0,0 +1,29 @@
+package sliddingwindow
+
+object Problem3090 {
+  class Solution {
+    fun maximumLengthSubstring(s: String): Int {
+      var l = 0
+      var r = 0
+      val map = hashMapOf<Char, Int>()
+      var max = 0
+
+      while (r < s.length) {
+        val rc = s[r]
+        val lc = s[l]
+
+        val rcCount = map[rc] ?: 0
+        if (rcCount < 2) {
+          map[rc] = rcCount + 1
+          r++
+        } else {
+          map[lc] = map[lc]!! - 1
+          l++
+        }
+        max = maxOf(max, r - l)
+      }
+
+      return max
+    }
+  }
+}
