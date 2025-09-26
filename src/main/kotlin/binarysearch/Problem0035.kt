18070ac28eebc61dc68200cf9efc71d927eaba79
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2025-09-25 21:06:10 -0300

    add 35

---
+++ src/main/kotlin/binarysearch/Problem0035.kt
@@ -0,0 +1,21 @@
+package binarysearch
+
+object Problem0035 {
+  class Solution {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+      var l = 0
+      var r = nums.lastIndex
+
+      while (l <= r) {
+        val i = l + (r - l) / 2
+        when {
+          nums[i] == target -> return i
+          nums[i] < target -> l = i + 1
+          else -> r = i - 1
+        }
+      }
+
+      return l
+    }
+  }
+}
