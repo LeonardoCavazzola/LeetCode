4c0c54c00ad8ed29805a374f4d925e885d99145f
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2025-09-29 18:55:39 -0300

    update 15

--- src/main/kotlin/twopointers/Problem0015.kt
+++ src/main/kotlin/twopointers/Problem0015.kt
@@ -6,25 +6,23 @@ object Problem0015 {
       nums.sort()
       val list = mutableListOf<List<Int>>()
 
-      for ((i, v) in nums.withIndex()) {
-        if (i > 0 && v == nums[i - 1]) continue
+      for ((i, n) in nums.withIndex()) {
+        if (i > 0 && n == nums[i - 1]) continue
 
         var l = i + 1
         var r = nums.lastIndex
 
         while (l < r) {
-          val sum = v + nums[l] + nums[r]
+          val sum = n + nums[l] + nums[r]
           when {
-            sum == 0 -> {
-              list.add(listOf(v, nums[l], nums[r]))
-              l++
-              while (nums[l] == nums[l - 1] && l < r) {
-                l++
-              }
-            }
-
+            sum < 0 -> l++
             sum > 0 -> r--
-            else -> l++
+            else -> {
+              list.add(listOf(n, nums[l], nums[r]))
+
+              do l++ while (l < r && nums[l] == nums[l - 1])
+              do r-- while (l < r && nums[l] == nums[r + 1])
+            }
           }
         }
       }
